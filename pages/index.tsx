b7b3d9b967fa9651938057ad5e1da401d412e80d
AVATAH <emmaogah97@gmail.com>
2020-01-24 14:08:48 +0100

    testing stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,2 +1,11 @@
-export { default } from './coming-soon'
+import {BlueNavBar} from "../components/Navbars"
 
+
+const Page = ()=>{
+    return <div>
+        <BlueNavBar/>
+        <h1>Index PAGE</h1>
+    </div>
+}
+
+export default Page
