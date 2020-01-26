164a42131fb6b31983a74e88eea3b12a276f2661
AVATAH <emmaogah97@gmail.com>
2020-01-26 12:53:36 +0100

    First Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,29 @@
 import {BlueNavBar} from "../components/Navbars"
+import { useState } from "react"
+
 
 
 const Page = ()=>{
+const [count,setCount] = useState(0)
+const [nameAbunga,setNameAbunga] = useState({
+    name:''
+})
+
+const increment = ()=>{
+    setCount(count+1)
+    setNameAbunga({
+        name:nameAbunga.name+count.toString()
+    })
+}
+
     return <div>
         <BlueNavBar/>
         <h1>Index PAGE</h1>
+<p>count value: {count}</p>
+<button onClick={increment}>Increment</button>
+<p>{nameAbunga.name}</p>
     </div>
 }
 
 export default Page
+
---
+++ pages/landing-page.tsx
@@ -0,0 +1,13 @@
+import Navbars from "../components/Navbars"
+
+
+
+
+const Page = () => {
+return <div>
+     <Navbars.BlueNavBar/>
+    
+</div>
+}
+
+export default Page
