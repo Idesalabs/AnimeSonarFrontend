0e7cf5bc7c3624575dce8ba3112a9de6f9fc5c25
AVATAH <emmaogah97@gmail.com>
2020-01-27 15:03:43 +0100

    Created landing-page and IntroSection component

---
+++ pages/landing-page.tsx
@@ -0,0 +1,19 @@
+const IntroSection =() => {
+    return <div>
+        <section>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing.</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur, repellendus! Tenetur, illum nulla.</p>
+        </section>
+        </div>
+}
+
+
+const LandingPage = () => {
+    return <div>
+        <IntroSection/>
+    </div>
+
+}
+
+
+export default LandingPage
