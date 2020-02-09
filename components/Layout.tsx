2403360ce384a5e397ef1126a26e0087f84b853a
AVATAH <emmaogah97@gmail.com>
2020-02-09 03:41:42 +0100

    Fixed the background image gradient

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,10 +3,26 @@ import Header from './Header'
 export default props => (
     <div>
         <Header/>
+        <div className="b-image__skewer"></div>
         {props.children}
         <style jsx>{`
         div {
-            margin: 20px 0;
+            margin: auto;
+            position: relative;
+            z-index: -2;
+            height: ;
+            width: 100%;
+            max-width: 700px;
+        }
+        .b-image__skewer {
+            height: 50vh;
+            background-image: linear-gradient(to bottom right, rgba(250, 74, 20, 0.7), rgba(250, 104, 20, 0.7)), url("https://picsum.photos/1000");
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+            clip-path: polygon(0% 0%, 100% 0%, 100% 50%, 0% 100%);
+            position: absolute;
+            z-index: -1;
         }
         `}</style>
     </div>
