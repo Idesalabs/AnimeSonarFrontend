62a6dac925209a34fe77e1cb9d6e34f6f9156cab
AVATAH <emmaogah97@gmail.com>
2020-02-13 10:08:00 +0100

    Adjusted background skewer

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,7 @@
 import Header from './Header'
 
 export default props => (
-    <div>
+    <div >
         <Header/>
         <div className="b-image__skewer"></div>
         <div className="children">
@@ -13,17 +13,19 @@ export default props => (
             position: relative;
             z-index: -2;
             width: 100vw;
-        }
-        .b-image__skewer {
-            height: 50vh;
+          }
+
+          .b-image__skewer {
+            height: 75vh;
             background-image: linear-gradient(to bottom right, rgba(250, 74, 20, 0.7), rgba(250, 47, 20, 0.7)), url("https://picsum.photos/1000");
             background-position: center;
             background-repeat: no-repeat;
             background-size: cover;
-            clip-path: polygon(0% 0%, 100% 0%, 100% 50%, 0% 100%);
+            clip-path: polygon(0% 0%, 100% 0%, 100% 40%, 0% 100%);
             position: absolute;
             z-index: -5;
-        }
+          }
+
         .children {
             margin: auto;
             width: 100%;
