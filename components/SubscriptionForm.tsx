bc62f0a2534820e96599e311f574bbc43d5fc367
AVATAH <emmaogah97@gmail.com>
2020-02-14 14:07:48 +0100

    Edited subscription form

--- components/SubscriptionForm.tsx
+++ components/SubscriptionForm.tsx
@@ -4,13 +4,14 @@ const SubsForm = () => (
     <div>
         <form action="">
             <label htmlFor="">Get notified once we launch</label>
-            <input type="email" className="subscription"/>
+            <input  type="email" className="subscription"/>
             <a href="#" className="subscription">SUBSCRIBE</a>
         </form>
         <style jsx>
             {`
             form {
                 font-size: 14px;
+                user-select: none;
             }
             label {
                 color: ${colors.primary};
@@ -18,16 +19,20 @@ const SubsForm = () => (
             }
             input {
                 border: none;
-                border-radius: 50px 0 0 50px ;
+                border-radius: 50px 0 0 50px;
+                border: 1px solid #ff552b80;
+               border-right-style:none;
             }
             a {
                 background: ${colors.button};
                 color: #fff;
                 font-weight: 700;
                 border-radius: 0 50px 50px 0;
+                border: 1px solid #ff552b80;
+                border-left-style:none;
+                
             }
             .subscription {
-                border: .5px solid ${colors.primary};
                 display: inline-block;
                 padding: 4px 22px;
             }
