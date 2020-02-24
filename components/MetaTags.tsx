266cabdf63fa238724e4b4c6be1cd54817f909ab
scroobius-pip <sim04ful@gmail.com>
2020-02-24 06:06:29 +0100

    added favicons

--- components/MetaTags.tsx
+++ components/MetaTags.tsx
@@ -19,6 +19,13 @@ const MetaTags = () => {
         <meta property="twitter:title" content="Anime Sonar - Over-Engineered Anime Suggestions" />
         <meta property="twitter:description" content="Get anime suggestions by expressing what you want to watch." />
         <meta property="twitter:image" content="/logo-orange.png"></meta>
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff"></meta>
     </Head>;
 }
 
