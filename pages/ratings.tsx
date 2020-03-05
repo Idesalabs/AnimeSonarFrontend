4b1bb720860ede026e53c69d1ca2e470be0c6430
AVATAH <emmaogah97@gmail.com>
2020-03-05 16:36:27 +0100

    Installed star ratings dependacy Added the Tag-ratings card Added the suggest subtags card

--- pages/ratings.tsx
+++ pages/ratings.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { useState } from 'react'
+import StarRatings from 'react-star-ratings';
 import Layout from '../components/Layout'
 import Section from '../components/Section'
 import BottomPadding from '../components/BottomPadding'
@@ -6,16 +8,109 @@ import { colors } from '../styles'
 import ShareButtons from '../components/ShareButtons';
 import { RatingsCard } from './../components/RatingsCard';
 import SearchBox from './../components/SearchBox';
-import Button from '../components/button'
+import Button from '../components/button';
+import { IconContext } from "react-icons";
+import { FaSearch } from 'react-icons/fa';
 
-export default () => (
-    <>
+export default () =>{
+    const [rating, setRating] = useState(0);
+    const [subTag, setSubTag] = useState('');
+
+
+    return <>
         <Layout>
            <div style = {{ maxWidth: '600px'}}>
                 <RatingsCard title = 'What was the last anime you watched?'>
-
                 <Section justifyContent= 'start' padding= '20px 0'>
                     <SearchBox height ='40px'/>
+                </Section>
+
+                <Section justifyContent= 'flex-end' padding= '10px 0 0' margin= '10px 0 0'>
+                    <Button title ='NEXT' borderRadius='50px' padding= '5px 0' width= '150px'/>
+                </Section>
+                </RatingsCard>
+                <RatingsCard title= 'Ratings'>
+                <Section justifyContent= 'start' padding= '20px 0'>
+                    <ul style={{ paddingLeft: 0}}>
+                        <li style={{ display: 'flex'}}>
+                            <p style={{ color: '#000', fontSize: '25px', fontWeight: 'bold', paddingRight: '25px'}}>Action</p>
+                            <StarRatings
+                            starRatedColor="blue"
+                            starDimension = '25px'
+                            numberOfStars={10}
+                            rating={rating}
+                            changeRating={setRating}
+                            name='Action'
+                            />
+                        </li>
+                    </ul>
+                </Section>
+                <Section justifyContent= 'flex-end' padding= '10px 0 0' margin= '10px 0 0'>
+                    <Button title ='NEXT' borderRadius='50px' padding= '5px 0' width= '150px'/>
+                </Section>
+                </RatingsCard>
+                <RatingsCard title = 'Suggest SubTags'>
+                <Section justifyContent= 'start' padding= '20px 0'>
+                <>
+            <form className='form'>
+            <IconContext.Provider value={{ style: {fontSize: '18px'} }}>
+                <input name='search-box' placeholder="Enter subTag" value={subTag} onChange={event => {
+                        setSubTag(event.target.value)
+                    }} className="search-input" />
+                <button type = "submit" className="search-button"><FaSearch/></button>
+            </IconContext.Provider>
+            </form>
+            <style jsx>
+            {`
+            *:focus {
+                outline: none;
+            }
+
+            ::placeholder {
+                color: ${colors.primary};
+                opacity: 0.7;
+            }
+
+            .form {
+                position: relative;
+                font-size: 15px;
+                //display: inline-block;
+            }
+            
+            .search-input {
+                width: 350px;
+                height: 40px;
+                padding-left: 15px;
+                border: 2px solid ${colors.primary};
+                border-radius: 10px;
+                font-weight: 500;
+                color: ${colors.primary};
+                transition: all 1s;
+            }
+
+            .search-input:focus {
+                background: ${colors.grey};
+            }
+
+            .search-button {
+                position: absolute;
+                top: 0;
+                right: 0;
+                height: 40px;
+                width: 40px;
+                border-radius: 50%;
+                border: none;
+                background: transparent;
+                color: ${colors.primary};
+                transition: all 0.5s;
+            }
+
+            .search-button:hover {
+                opacity: 0.7;
+            }
+            `}
+            </style>
+            </>
                 </Section>
 
                 <Section justifyContent= 'flex-end' padding= '10px 0 0' margin= '10px 0 0'>
@@ -26,4 +121,4 @@ export default () => (
         <BottomPadding/>
         </Layout>
     </>
-)
+    }
