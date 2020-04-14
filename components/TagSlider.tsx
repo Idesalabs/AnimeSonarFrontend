5622dd240016bc420adeb6f3d3230951acb24e8f
scroobius-pip <sim04ful@gmail.com>
2020-04-14 20:54:35 +0100

    tag slider changed 4 tags to 3

--- components/TagSlider.tsx
+++ components/TagSlider.tsx
@@ -13,10 +13,10 @@ const _tags = [
         name: 'Comedy',
         color: 'orange'
     },
-    {
-        name: 'Horror',
-        color: 'black'
-    },
+    // {
+    //     name: 'Horror',
+    //     color: 'black'
+    // },
 
 
 ]
