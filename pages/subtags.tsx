a98bf315bbec6f9a2192836e8c6b294f08d64029
scroobius-pip <sim04ful@gmail.com>
2020-04-14 19:50:46 +0100

    changed react-tag-autocomplete to latest version that's not beta

--- pages/subtags.tsx
+++ pages/subtags.tsx
@@ -2,50 +2,50 @@ import { useState } from 'react'
 import ReactTags from 'react-tag-autocomplete'
 
 export default () => {
-    const [subTags, setSubTags] = useState(
-        [
-            { id: 1, name: "Apples" },
-            { id: 2, name: "Pears" }
-          ]
-    );
-    const [suggestions, setSuggestions] = useState(
-      [
-        { id: 3, name: "Bananas" },
-        { id: 4, name: "Mangos" },
-        { id: 5, name: "Lemons" },
-        { id: 6, name: "Apricots" }
-      ]
-    );
+  const [subTags, setSubTags] = useState(
+    [
+      { id: 1, name: "Apples" },
+      { id: 2, name: "Pears" }
+    ]
+  );
+  const [suggestions, setSuggestions] = useState(
+    [
+      { id: 3, name: "Bananas" },
+      { id: 4, name: "Mangos" },
+      { id: 5, name: "Lemons" },
+      { id: 6, name: "Apricots" }
+    ]
+  );
 
-    
-   function onDelete (i) {
+
+  function onDelete(i) {
     const subtags = subTags.slice(0);
     subtags.splice(i, 1);
     setSubTags(subtags);
   }
 
-  function onAddition (subTag) {
+  function onAddition(subTag) {
     const subtags = [].concat(subTags, subTag);
     setSubTags(subtags);
   }
 
   return (
-      <>
-        <ReactTags
-            tags={subTags}
-            suggestions={suggestions}
-            onDelete={onDelete}
-            onAddition={onAddition}
-            allowNew={true}
-            classNames= 'color' />
-          <style jsx>
-            {`
+    <>
+      <ReactTags
+        tags={subTags}
+        suggestions={suggestions}
+        onDelete={onDelete}
+        onAddition={onAddition}
+        allowNew={true}
+      />
+      <style jsx>
+        {`
             .color {
               color: blue;
             }
             `}
-          </style>
-      </>
+      </style>
+    </>
   )
-   
+
 }
