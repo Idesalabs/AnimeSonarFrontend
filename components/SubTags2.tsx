ff93df5702153e6f352001daa819e5497d192d44
Emmaous <emmaogah97@gmail.com>
2020-04-24 16:28:42 +0100

    react-select subtag updates

--- components/SubTags2.tsx
+++ components/SubTags2.tsx
@@ -1,6 +1,7 @@
 import Select, { components } from 'react-select';
 import makeAnimated from 'react-select/animated';
-import { FaSearch } from 'react-icons/fa';
+import { colors } from '../styles'
+import { IoIosCloseCircle } from "react-icons/io";
 
 export default () => {
     
@@ -11,22 +12,49 @@ export default () => {
       ];
 
       
-      const DropdownIndicator = (props) => {
+    const DropdownIndicator = (props) => { 
           return (
               <components.DropdownIndicator {...props}>
-            <FaSearch/>
+            <div/>
           </components.DropdownIndicator>
         );
     };
+      
+    const IndicatorSeparator = ({ props }) => {
+        return <div/>;
+    }
+
+    const MultiValueRemove = (props) => {
+        return (
+            <components.MultiValueRemove {...props}>
+                <IoIosCloseCircle/>
+            </components.MultiValueRemove>
+        )
+    }
     
     const animated = makeAnimated();
 
     const styles = {
-        control: (currentStyles) => ({
+        control: (currentStyles, state) => ({
             ...currentStyles,
             border: 'none',
             borderRadius: 0,
             borderBottom: '2.5px solid #FF552B',
+        }),
+        multiValue: (currentStyles, state) => ({
+            ...currentStyles,
+            backgroundColor: `${colors.primary}`,
+        }),
+        multiValueLabel: (currentStyles, state) => ({
+            ...currentStyles,
+            color: 'white',
+        }),
+        multiValueRemove: (currentStyles, state) => ({
+            ...currentStyles,
+            color: '#fff',
+            ':hover': {
+                color: `${colors.primary}`,
+            },
         })
     }
 
@@ -37,7 +65,7 @@ export default () => {
         name='sweets'
         isMulti
         components={{
-            ...animated,DropdownIndicator}}
+            ...animated,DropdownIndicator,IndicatorSeparator,MultiValueRemove}}
         styles={styles} />
         </>
     )
