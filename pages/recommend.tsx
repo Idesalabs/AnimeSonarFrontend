d63a2196a3acd5a1b18a955eaf6ee5d385824b4d
Emmaous <emmaogah97@gmail.com>
2020-05-27 21:20:35 +0100

    Tushed up recommends page

--- pages/recommend.tsx
+++ pages/recommend.tsx
@@ -1,14 +1,59 @@
 import Layout from '../components/Layout'
-import Card from '../components/Card'
+import { RatingsCard } from './../components/RatingsCard';
 import TagSlider from '../components/TagSlider'
+import SubTags2 from '../components/SubTags2';
+import { useState } from 'react'
 
 export default () => {
+    const [recommendPageState, setRecommendPageState] = useState()
+
     return <Layout noSlant>
-        <Card showLid>
-            <h2>Select Tags</h2>
+        <RatingsCard title='Select Tags'>
             <p>Adjust the sliders to change how much of that tag should be in the anime.</p>
             <TagSlider />
 
-        </Card>
+        </RatingsCard>
+
+        <RatingsCard title='Enter SubTags'>
+            
+            {/* <SubTags2
+                    initialTags= {ratingPageState.selectedAnime.subTags}
+                    onCreate={(input) => {
+                        const newSubTag = createOption(input);
+                        setRatingPageState({
+                            ...ratingPageState,
+                            selectedAnime: {
+                                ...ratingPageState.selectedAnime,
+                                subTags: [
+                                    ...ratingPageState.selectedAnime.subTags,
+                                    newSubTag
+                                ]
+                            }
+                        })}
+                    }
+                    /> */}
+
+        </RatingsCard>
+
+        <RatingsCard title='Blacklist Tags'>
+            
+            {/* <SubTags2
+                    initialTags= {ratingPageState.selectedAnime.subTags}
+                    onCreate={(input) => {
+                        const newSubTag = createOption(input);
+                        setRatingPageState({
+                            ...ratingPageState,
+                            selectedAnime: {
+                                ...ratingPageState.selectedAnime,
+                                subTags: [
+                                    ...ratingPageState.selectedAnime.subTags,
+                                    newSubTag
+                                ]
+                            }
+                        })}
+                    }
+                    /> */}
+
+        </RatingsCard>
     </Layout>
 }
